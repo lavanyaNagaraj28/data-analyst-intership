c06da8c5734ea3eab1ca6d82bc7d95276e430dec
lavanyaNagaraj28 <lavanyanagaraj2807@gmail.com>
2025-10-02 21:16:25 +0530

    Add files via upload

---
+++ task-5/sql.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+conn= sqlite3.connect("sales_data.db")
+query="""
+SELECT product,
+    SUM(quantity) As total_qty,
+    SUM(quantity * price) AS revenue
+FROM sales
+GROUP BY product
+"""
+df=pd.read_sql_query(query,conn)
+print(df)
+df.plot(kind='bar',x='product',y='revenue',legend=False, title="Revenue by product")
+plt.ylabel("Revenue")
+plt.savefig("sales_chart.png")
+plt.show()
+conn.close()
